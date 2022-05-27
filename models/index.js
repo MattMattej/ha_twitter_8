11deb7d00c2b7ac97e1d4921cb9053ee43268651
Santiago Fantoni <fantonisantiago@gmail.com>
2022-05-27 20:19:52 -0300

    cambio del nombre de la base de datos

--- models/index.js
+++ models/index.js
@@ -1,26 +1,13 @@
-//const mongoose = require("mongoose");
-//const dbConnect = async () => {
-//await mongoose.connect("mongodb://localhost/twitter_8");
-// mongoose.connection;
-//    .once("open", () => {
-//     console.log("[Database] Se ha establecido la conexion a la bd");
-//    })
-//    .on("error", (error) => {
-//     console.log(error);
-//    });
-//};
-//module.exports = dbConnect;
-
 const mongoose = require("mongoose");
 
 const dbInit = () => {
-  mongoose.connect("mongodb://localhost/dbmongoose-hack-academy");
+	mongoose.connect("mongodb://localhost/twitter_8");
 
-  mongoose.connection
-    .once("open", () =>
-      console.log("¡Conexión con la base de datos establecida!")
-    )
-    .on("error", (error) => console.log(error));
+	mongoose.connection
+		.once("open", () =>
+			console.log("¡Conexión con la base de datos establecida!")
+		)
+		.on("error", (error) => console.log(error));
 };
 
 module.exports = dbInit;
