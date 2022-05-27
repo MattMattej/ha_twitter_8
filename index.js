4b78a2985bbabeb208748b6f68555d1446b70037
MattMattej <mateo.casciotti@gmail.com>
2022-05-27 15:32:36 -0300

    setupinicial

--- index.js
+++
@@ -1,7 +0,0 @@
-const passport = require("passport");
-const session = require("./config/sessionConfig");
-//Seteamos el middleware session de manera global con propiedades
-app.use(session);
-//otro middleware, especificamos a passport que vamos a usar session.
-app.use(passport.session());
-require("./config/passportConfig");
