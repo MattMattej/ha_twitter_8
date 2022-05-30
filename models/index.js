4dd363d8990ff5a7bf736e60e1c1c69ad29658d5
Santiago Fantoni <fantonisantiago@gmail.com>
2022-05-30 14:07:03 -0300

    Cambios menores
    -Comentar la linea 6 de rutas privadas
    -cambiar la coneccion por .ENV
    -Eliminar unos require que no se usaban

--- models/index.js
+++ models/index.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
-const userSeeder = require("../seeders/seederUsuarios");
-const User = require("./User");
 
 const dbInit = async () => {
-  mongoose.connect("mongodb://localhost/twitter_8");
+	mongoose.connect(process.env.DB_CONNECTION);
 
-  mongoose.connection
-    .once("open", () =>
-      console.log("¡Conexión con la base de datos establecida!")
-    )
-    .on("error", (error) => console.log(error));
+	mongoose.connection
+		.once("open", () =>
+			console.log("¡Conexión con la base de datos establecida!")
+		)
+		.on("error", (error) => console.log(error));
 };
 
 module.exports = dbInit;
