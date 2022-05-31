a645284871504c3fc3c187c54ce574f996aac38f
goldvinda <104211952+goldvinda@users.noreply.github.com>
2022-05-31 13:12:49 -0300

    Cambios botón like

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,14 @@
-console.log("Client-side code running");
-
-const button = document.getElementById("like");
+const button = document.getElementById("favTweet");
 button.addEventListener("click", function (e) {
+  e.preventDefault();
   console.log("button was clicked");
 
-  fetch("/favTweet", { method: "POST" })
+  const tweetId = e.dataset.tweetid;
+  fetch(`/favTweet/${tweetId}$`, { method: "POST" })
     .then(function (response) {
       if (response.ok) {
         console.log("click was recorded");
-        return;
+        return tweetId;
       }
       throw new Error("Request failed.");
     })
--- routes/routesPrivate.js
+++ routes/routesPrivate.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/privateController");
 const isAuthenticated = require("../middlewares/isAuthenticated");
+const Tweet = require("../models/Tweet"); //se va ahora
 
 router.use(isAuthenticated);
+
 //pagina de perfil
 router.get("/profile", (req, res) => {
   res.render("profile", { user: req.user });
 });
 
 //boton de like
-router.post("/favTweet", controller.favTweet);
+router.post("/favTweet/:id", async function (req, res) {
+  res.json(await Tweet.findById({ _id: req.params.id }));
+});
 
 //crear un tweet
 router.get("/crear", controller.renderNewTweet);
